21919b0cd537466a9b057637cc2a9cec45842e5b
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-09-06 22:58:26 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,5 +1,11 @@
 def run_guessing_game
   input = gets.chomp
+  computer_num = rand(1..6)
+  until input == "exit"
+  if input != computer_num
+    puts "Sorry! The computer guessed #{computer_num}."
+      end
+    end
   if input == "exit"
       puts "Goodbye!"
     end
