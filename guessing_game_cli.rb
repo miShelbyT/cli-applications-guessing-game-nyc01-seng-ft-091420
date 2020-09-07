403eed3f33b7d1e0d5c2c85472e8cd0905c70aed
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-09-07 02:06:11 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,5 +1,5 @@
 def run_guessing_game
-  input = gets.chomp
+  input = gets.chomp.to_i
   if input == "exit"
     puts "Goodbye!"
   end
@@ -9,6 +9,14 @@ def run_guessing_game
     puts "Sorry! The computer guessed #{computer_num}."
   elsif input == computer_num
     puts "You guessed the correct number!"
+<<<<<<< HEAD
   end
   input
+=======
+      end
+    end
+  if input == "exit"
+      puts "Goodbye!"
+    end
+>>>>>>> 12edd1a622fe77a856045f74cd4dee2d2f267f71
 end
