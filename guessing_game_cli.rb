fd3f93b91d8e23168f9ab8c44964787477f5033b
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-09-08 18:57:25 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,8 @@
 def run_guessing_game
   computer_num = rand(7)
   input = gets.chomp
-  # input == "exit" ? (puts "Goodbye!") : input2 = input
+  if input == "exit"
+    puts "Goodbye!"
+  end
   input.to_i == computer_num ? (puts "You guessed the correct number!") : (puts "Sorry! The computer guessed #{computer_num}.")
 end
